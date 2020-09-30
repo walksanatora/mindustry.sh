c1f472c002225f60c7589c999190e76acc16667e
walksanatora <walkerffo22@hotmail.com>
2020-09-30 13:04:09 -0700

    umm

--- mindustry.sh
+++ mindustry.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
+
 if [ `whoami` != 'root' ]
   then
-    echo "need root to setup"
-    exit 1
+    echo "You must be root to do this. (/bin is used)"
+    exit
 fi
-mind = $(pwd)
+mind=$(pwd)
 cd /home/$(logname)/.config/
 mkdir mindustry
 cd /home/$(logname)/.local/share
@@ -12,5 +13,6 @@ mkdir Mindustry
 cd Mindustry
 mkdir builds
 cd $mind
-cp -a /bin/. /usr/bin
+cp -a $(pwd)/bin /bin
+read
 
