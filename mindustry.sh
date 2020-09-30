6cc96f8c518b9b7b64ea5a5f6277015ded3b9223
walksanatora <walkerffo22@hotmail.com>
2020-09-30 12:51:06 -0700

    updoot

--- mindustry.sh
+++ mindustry.sh
@@ -4,9 +4,13 @@ if [ `whoami` != 'root' ]
     echo "need root to setup"
     exit 1
 fi
-mind = (pwd)
+mind = $(pwd)
 cd /home/$(logname)/.config/
 mkdir mindustry
+cd /home/$(logname)/.local/share
+mkdir Mindustry
+cd Mindustry
+mkdir builds
 cd $mind
 cp -a /bin/. /usr/bin
 
